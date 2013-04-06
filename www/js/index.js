ea227286cd1cdee660d7e3a65735e9ef817d2ee6
Raimo Launonen <Raimo_Launonen@hotmail.com>
2013-04-06 22:00:17 +0300

    js chang ; back

--- www/js/index.js
+++ www/js/index.js
@@ -39,23 +39,23 @@ var app = {
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
     }
-}
+};
 function countspeed() {
 	alert("Yksi");
 	countdestime();
        	document.getElementById("speeda").value=document.getElementById("lega").value/document.getElementById("starttah").value;
-}
+};
 function counttime() {
        	document.getElementById("starttah").value=document.getElementById("lega").value/document.getElementById("speeda").value;
 	countinttime();
-}
+};
 function countleg() {
 	countdestime();
        	document.getElementById("lega").value=document.getElementById("starttah").value/document.getElementById("speeda").value;
-}
+};
 function countdestime() {
        	document.getElementById("starttah").value=document.getElementById("starttah").value+(document.getElementById("starttam").value/60.0)+(document.getElementById("starttas").value/3600.0);
-}
+};
 function countinttime() {
        	var hh=Math.floor(document.getElementById("starttah").value);
 	var mmmm=60*(document.getElementById("starttah").value-hh);
