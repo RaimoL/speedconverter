77fd2ca05380cebe118132a6993dc7b8692a8b0a
Raimo Launonen <Raimo_Launonen@hotmail.com>
2013-04-07 00:16:38 +0300

    one too many )

--- www/js/index.js
+++ www/js/index.js
@@ -42,7 +42,7 @@ var app = {
 };
 function countspeed() {
 	countdestime();
-	document.getElementById("speeda").value=document.getElementById("lega").value)/document.getElementById("starttah").value;
+	document.getElementById("speeda").value=document.getElementById("lega").value/document.getElementById("starttah").value;
 	document.getElementById("starttah").value=Math.floor(document.getElementById("starttah").value);
 };
 /*
