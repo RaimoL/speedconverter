4459d8175d8d3c3e0b4816b4c0bae979755b1488
Raimo Launonen <Raimo_Launonen@hotmail.com>
2013-04-07 00:37:03 +0300

    km=km/h*time

--- www/js/index.js
+++ www/js/index.js
@@ -52,7 +52,7 @@ function counttime() {
 };
 function countleg() {
  	countdestime();
-       	document.getElementById("lega").value=document.getElementById("starttah").value/document.getElementById("speeda").value;
+       	document.getElementById("lega").value=document.getElementById("starttah").value*document.getElementById("speeda").value;
 	document.getElementById("starttah").value=Math.floor(document.getElementById("starttah").value);
 };
 function countdestime() {
